52b102612fc12dbdcd1aaee37ec9e88d83930cb6
Jacques Gignoux <jacques.gignoux@upmc.fr>
2019-02-20 11:38:11 +0100

    refactored the versioning system

--- uit/scripts/fr/cnrs/iees/versioning/VersionManager.java
+++ uit/scripts/fr/cnrs/iees/versioning/VersionManager.java
@@ -1,32 +1,12 @@
-/**************************************************************************
- *  UIT - a Universal Indexing Tree                                       *
- *                                                                        *
- *  Copyright 2018: Jacques Gignoux & Ian D. Davies                       *
- *       jacques.gignoux@upmc.fr                                          *
- *       ian.davies@anu.edu.au                                            * 
- *                                                                        *
- *  UIT is a generalisation and re-implementation of QuadTree and Octree  *
- *  implementations by Paavo Toivanen as downloaded on 27/8/2018 on       *
- *  <https://dev.solita.fi/2015/08/06/quad-tree.html>                     *
- *                                                                        *
- **************************************************************************                                       
- *  This file is part of UIT (Universal Indexing Tree).                   *
- *                                                                        *
- *  UIT is free software: you can redistribute it and/or modify           *
- *  it under the terms of the GNU General Public License as published by  *
- *  the Free Software Foundation, either version 3 of the License, or     *
- *  (at your option) any later version.                                   *
- *                                                                        *
- *  UIT is distributed in the hope that it will be useful,                *
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of        *
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
- *  GNU General Public License for more details.                          *                         
- *                                                                        *
- *  You should have received a copy of the GNU General Public License     *
- *  along with UIT.  If not, see <https://www.gnu.org/licenses/gpl.html>. *
- *                                                                        *
- **************************************************************************/
 package fr.cnrs.iees.versioning;
+
+/**********************************************************************************************
+ *                                                                                            *
+ *   GENERATED FILE - DO NOT EDIT !                                                           * 
+ *                                                                                            *
+ *   if you need to modify this file, use the GenerateVersionManager class in library omhtk   *
+ *                                                                                            *
+ **********************************************************************************************/
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
@@ -38,6 +18,7 @@ import java.io.InputStreamReader;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Date;
+import static fr.cnrs.iees.versioning.VersionSettings.*;
 
 import static java.nio.file.StandardCopyOption.*;
 
@@ -66,58 +47,6 @@ import static java.nio.file.StandardCopyOption.*;
  *
  */
 public class VersionManager {
-	
-	// Change these fields to suit the project ====================================================
-	
-	/** The organisation name as will appear in the ivy module specification - it is a good idea
-	 * to keep it consistent with the project src directory (although not required).*/
-	private static String ORG = "fr.cnrs.iees";
-	
-	/** The name of the ivy module (this will be the name of the generated jar file for 
-	 * dependent projects).*/
-	private static String MODULE = "uit";
-	
-	/** The ivy status of the module: integration, milestone, or release are the ivy defaults
-	 * But we can define ours like bronze, gold, silver, or crap, supercrap, ultracrap. */
-	private static String STATUS = "integration";
-	
-	/** The license under which this module (= jar) is distributed */
-	private static String LICENSE = "gpl3";
-	
-	/**The url to the text of the license */
-	private static String LICENSE_URL = "https://www.gnu.org/licenses/gpl-3.0.txt";
-	
-	/**A (long) description of the ivy module */
-	private static String DESCRIPTION = 
-		"This module implements a kd-tree, i.e. a generalisation of a QuadTree to any dimension." +
-		" It can be used to improve performance when searching objects in a large space.";
-	
-	/**
-	 * <p>Dependencies on other modules (they will be integrated in the ivy script).</p>
-	 * 
-	 * <p>This is a (n * 3) table of Strings.<br/>
-	 * Every line is a new dependency.
-	 * On every line, the 3 Strings must match the ivy fields 'org' (for organisation), 
-	 * 'name' (for the module name), and 'rev' (for the revision or version number). The '+' can
-	 * be conveniently used to specify 'any version'.
-	 * The field can be empty (just needs the external braces).<br/>
-	 * Example value: 
-	 * <pre>{{"org.galaxy.jupiter","crap","1.0.+"},
-	 * {"org.ocean.lostIsland","strungk","3.12.254"}}</pre> </p>
-	 * <p>Wildcards for revision numbers are indicated <a href="http://ant.apache.org/ivy/history/master/ivyfile/dependency.html">there</a>.</p>
-	 * 
-	 */
-	private static String[][] DEPS = { 
-		{"fr.ens.biologie", "generics", "+"},
-		{"org.graphstream", "gs-ui", "1.3"}
-	};
-	
-	/** The name of the main class to put in the jar manifest, if any. This enables users to
-	 * run the jar using this class as the entry point. Of course this must be a fully qualified
-	 * valid java class name found in the jar. 
-	 */
-	private static String MAINCLASS = null;
-
 	// ============================================================================================
 	
 	// This shouldnt be changed ===================================================================
@@ -164,23 +93,36 @@ public class VersionManager {
 	
 	private static String build1 =
 	"<project xmlns:ivy=\"antlib:org.apache.ivy.ant\" basedir=\"..\" default=\"publishJar\">\n\n" +
+
 	"\t<!-- Artifacts are produced in the project lib directory-->\n" +
 	"\t<property name=\"jarRepo\" location=\"${user.dir}/../lib\"/>\n\n" +
+	"\t<property name=\"docRepo\" location=\"${user.dir}/../doc/javadoc\"/>" +
+
 	"\t<!-- Use this string as the organisation package -->\n" +
 	"\t<property name=\"org\" value=\"" + ORG +"\"/>\n\n" +
+
 	"\t<!-- The project name -->\n" +
 	"\t<property name=\"project\" value=\""+ MODULE +"\"/>\n\n" +
+
 	"\t<!-- Append the project name to the organisation string -->\n" +
 	"\t<property name=\"jarlib\" location=\"${jarRepo}/${org}.${project}\"/>\n\n" +
+
 	"\t<!-- Tasks -->\n" +
-	"\t<target name=\"makeArtifactDir\" description=\"create directory for jars\">\n" +
+	"\t<target name=\"makeArtifactDir\" description=\"create directory for jars and doc\">\n" +
 	"\t\t<mkdir dir=\"${jarlib}\"/>\n" +
+	"\t\t<mkdir dir=\"${docRepo}\"/>" +
 	"\t</target>\n\n" +
+
+	"\t<target name=\"cleanDoc\" description=\"cleanup javadoc directory\">\n" + 
+	"\t\t<delete dir=\"${docRepo}\"/>\n" + 
+	"\t</target>\n" + 
+
 	"\t<target name=\"resolve\" description=\"resolve dependencies and setup publication environment\">\n" +
 	"\t\t<ivy:configure/>\n" +
 	"\t\t<ivy:resolve file=\"scripts/${ivy.dep.file}\" conf=\"${ivy.configurations}\"/>\n" +
 	"\t\t<ivy:retrieve pattern=\"${ivy.retrieve.pattern}\" conf=\"${ivy.configurations}\"/>\n" +
 	"\t</target>\n\n" +
+
 	"\t<target name=\"makeJar\" description=\"pack as a jar library\" depends=\"makeArtifactDir,resolve\">\n" +
 	"\t\t<jar destfile=\"${jarlib}/${project}.jar\">\n" + 
 	"\t\t\t<fileset dir=\"bin\" \n" + 
@@ -199,11 +141,19 @@ public class VersionManager {
 	"\t\t\t</manifest>\n" + 
 	"\t\t</jar>\n" + 
 	"\t</target>\n\n" +
+
 	"\t<target name=\"publishJar\" description=\"make jar library available to others\" depends=\"makeJar\">\n" +
 	"\t\t<ivy:publish resolver=\"local\" overwrite=\"true\"  forcedeliver=\"true\">\n" +
 	"\t\t\t<artifacts pattern=\"${jarlib}/[artifact].[ext]\"/>\n" +
 	"\t\t</ivy:publish>\n" +
 	"\t</target>\n\n" +
+
+	"\t<target name=\"javadoc\" description=\"generate JavaDoc\" depends=\"makeArtifactDir,cleanDoc\">\n" + 
+	"\t\t<javadoc destdir=\"${docRepo}\">\n" + 
+	"\t\t\t<fileset dir=\"src\"/>\n" + 
+	"\t\t</javadoc>\n" + 
+	"\t</target>\n" + 
+
 	"</project>\n";
 	
 	private static String buildDependencyList() {
