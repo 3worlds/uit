6b14bf2a83ec6b7d36914a5816140de62b70ea1a
Jacques Gignoux <jacques.gignoux@upmc.fr>
2022-04-06 14:49:04 +0200

    updating license header in all files

--- uit/src/fr/cnrs/iees/uit/indexing/ExpandingLimitedPrecisionIndexingTree.java
+++ uit/src/fr/cnrs/iees/uit/indexing/ExpandingLimitedPrecisionIndexingTree.java
@@ -1,3 +1,31 @@
+/**************************************************************************
+ *  UIT - a Universal Indexing Tree                                       *
+ *                                                                        *
+ *  Copyright 2018: Jacques Gignoux & Ian D. Davies                       *
+ *       jacques.gignoux@upmc.fr                                          *
+ *       ian.davies@anu.edu.au                                            * 
+ *                                                                        *
+ *  UIT is a generalisation and re-implementation of QuadTree and Octree  *
+ *  implementations by Paavo Toivanen as downloaded on 27/8/2018 on       *
+ *  <https://dev.solita.fi/2015/08/06/quad-tree.html>                     *
+ *                                                                        *
+ **************************************************************************                                       
+ *  This file is part of UIT (Universal Indexing Tree).                   *
+ *                                                                        *
+ *  UIT is free software: you can redistribute it and/or modify           *
+ *  it under the terms of the GNU General Public License as published by  *
+ *  the Free Software Foundation, either version 3 of the License, or     *
+ *  (at your option) any later version.                                   *
+ *                                                                        *
+ *  UIT is distributed in the hope that it will be useful,                *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *  GNU General Public License for more details.                          *                         
+ *                                                                        *
+ *  You should have received a copy of the GNU General Public License     *
+ *  along with UIT.  If not, see <https://www.gnu.org/licenses/gpl.html>. *
+ *                                                                        *
+ **************************************************************************/
 package fr.cnrs.iees.uit.indexing;
 
 import fr.cnrs.iees.uit.space.Box;
