47eedc585eec0b9428570f2ff643e20b024cfafd
Jacques Gignoux <jacques.gignoux@upmc.fr>
2022-10-20 13:32:27 +0200

    dependency consistency

--- uit/scripts/fr/cnrs/iees/versioning/VersionSettings.java
+++ uit/scripts/fr/cnrs/iees/versioning/VersionSettings.java
@@ -83,7 +83,7 @@ public class VersionSettings {
 	 *
 	 */
 	protected static String[][] DEPS = {
-		{"fr.cnrs.iees.omhtk", "generics", "[0.4.0,)", null},
+		{"fr.cnrs.iees.omhtk", "generics", "[0.5.0,)", null},
 		//{"org.graphstream", "gs-ui", "1.3", null} // too heavy, removed
 	};
 
